462dab6ca7df7d17988cf70734e705afb7eb6bda
Danielx64 <289220+Danielx64@users.noreply.github.com>
2023-06-10 19:54:39 +1000

    Add RemoveSpecialChars()

--- PAppsplayer/PAppsplayer/MainWindow.xaml.cs
+++ PAppsplayer/PAppsplayer/MainWindow.xaml.cs
@@ -274,5 +274,23 @@ namespace PAppsplayer
 				}
 			}
 		}
+		public static string RemoveSpecialChars(string str)
+		{
+
+			str = str.Replace($"{Globals.URI_SCHEMA}", "");
+			// Create  a string array and add the special characters you want to remove
+			string[] chars = new string[] { "~", "`", "!", "@", "#", "$", "%", "^", "*", "(", ")", "_", "+", "}", "{", "]", "[", "|", "\"", ":", "'", ":", ">", "<", "/", ".", ",", "\\" };
+
+			//Iterate the number of times based on the String array length.
+			for (int i = 0; i < chars.Length; i++)
+			{
+				if (str.Contains(chars[i]))
+				{
+					str = str.Replace(chars[i], "");
+				}
+			}
+
+			return str;
+		}
 	}
 }
