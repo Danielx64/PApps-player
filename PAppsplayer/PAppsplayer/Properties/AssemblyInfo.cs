729af219559415191e41a97a391bd95d6f7e359d
Danielx64 <289220+Danielx64@users.noreply.github.com>
2023-06-10 21:11:20 +1000

    Fix launching tags

--- PAppsplayer/PAppsplayer/Properties/AssemblyInfo.cs
+++ PAppsplayer/PAppsplayer/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebView2TabTestWpfGC")]
+[assembly: AssemblyTitle("PAppsplayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebView2TabTestWpfGC")]
+[assembly: AssemblyProduct("PAppsplayer")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- PAppsplayer/PAppsplayer/Properties/Resources.Designer.cs
+++ PAppsplayer/PAppsplayer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WebView2TabTestWpfGC.Properties
+namespace PAppsplayer.Properties
 {
 
 
